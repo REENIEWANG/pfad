b8281a454b5b41be39f39e6da9242945a47a146e
REENIEWANG <REENIEWJR@GMAIL.COM>
2024-09-20 18:52:28 +0800

    assignment01
    20240920

--- assignment/main.py
+++ assignment/main.py
@@ -48,40 +48,32 @@ print(df)
 
 fig, ax = plt.subplots(figsize=(10, 5))
 
-# 设置背景颜色
 fig.patch.set_facecolor('black')
 ax.set_facecolor('black')
 
-# 绘制折线图
+
 ax.plot(df['Date'], df['Value'], marker='o', linestyle=':', color='#FFC0CB', linewidth=3)
 
-# 设置图表标题和标签
 ax.set_title('ASSIGNMENT01', fontsize=20, color='white')
 ax.set_xlabel('Date', fontsize=10, color='white')
 ax.set_ylabel('Value', fontsize=10, color='white')
 
-# 设置网格线
 ax.grid(True, which='both', linestyle='--', linewidth=1, color='white')
 
-# 设置日期格式和间隔
 ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%m-%d'))
 ax.xaxis.set_major_locator(plt.matplotlib.dates.DayLocator(interval=2))
 
-# 设置刻度颜色
 ax.tick_params(axis='x', colors='white')
 ax.tick_params(axis='y', colors='white')
 
-# 设置轴标签颜色
 ax.xaxis.label.set_color('white')
 ax.yaxis.label.set_color('white')
 
-# 设置刻度线颜色
 ax.spines['bottom'].set_color('white')
 ax.spines['top'].set_color('white')
 ax.spines['left'].set_color('white')
 ax.spines['right'].set_color('white')
 
-# 显示图表
 plt.show()
 
 
